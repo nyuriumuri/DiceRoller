180d78abea756c58d0c28008bd252f0e762661b8
nyuriumuri <nyuriumuri@gmail.com>
2021-08-05 00:02:59 +0200

    Updates

---
+++ static/js/54.0e8e2e76.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpDiceRoller=this.webpackJsonpDiceRoller||[]).push([[54],{118:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(12),o=e(8),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=54.0e8e2e76.chunk.js.map
